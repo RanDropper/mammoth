98d8e342e5f07089ad9023b53d59a37692b6966c
DiggerWang <wh19961111@163.com>
2023-01-02 16:27:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name = "mammoth",
       author_email= "randropper001@gmail.com",
       url = "https://github.com/RanDropper/mammoth",
       description = "An advanced time series prediction framework.",
-      packages = "mammoth",
+      packages = ["mammoth"],
       download_url = "https://github.com/RanDropper/mammoth.git",
       install_requires = ["tensorflow>=2.3.0"])
